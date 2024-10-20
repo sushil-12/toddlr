7925e8ac842c6994597039b64783dcf7a781c094
sushil <sushil124maurya@gmail.com>
2024-10-20 12:34:28 +0530

    added comment sd

--- src/logger.js
+++ src/logger.js
@@ -15,6 +15,15 @@ const customFormat = printf(({ level, message, timestamp }) => {
 const logDir = process.env.APP_ENV === 'local' ? path.join(__dirname, 'logs') : '/tmp/logs';
 const logFilePath = path.join(logDir, 'tofflr-cms-%DATE%.log');
 
+
+
+// Ensure the directories exist
+try {
+    fs.mkdirSync(logDir, { recursive: true });
+} catch (err) {
+    console.error('Error creating log directories:', err);
+}
+
 // Create general logger instance
 const logger = createLogger({
     level: 'error', // Log only error level and below
@@ -44,6 +53,13 @@ const logger = createLogger({
 // Create contact logger instance
 const contactLogDir = process.env.APP_ENV === 'local' ? path.join(__dirname, 'logs', 'contact') : '/tmp/logs/contact';
 const contactLogFilePath = path.join(contactLogDir, 'contact-api-%DATE%.log');
+console.log("contactLogFilePath", contactLogFilePath,   process.env.APP_ENV )
+// Ensure the contact log directory exists
+try {
+    fs.mkdirSync(contactLogDir, { recursive: true });
+} catch (err) {
+    console.error('Error creating contact log directory:', err);
+}
 
 // Create contact logger instance
 const contactLogger = createLogger({
@@ -60,6 +76,14 @@ const contactLogger = createLogger({
             maxSize: '20m', // 20 megabytes
             maxFiles: '14d' // Keep logs for 14 days
         }),
+        // Uncomment to add console transport for debugging
+        // new transports.Console({
+        //     level: 'info', // Adjust log level as needed
+        //     format: combine(
+        //         timestamp(),
+        //         customFormat
+        //     )
+        // })
     ],
 });
 
