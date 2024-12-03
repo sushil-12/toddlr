4360ddf9ae9f34728a21ae0be07c70e24eaf67ca
alpha2135 <begin2135@gmail.com>
2024-12-04 01:50:54 +0530

    schema for bundle and route for creating bundle

---
+++ src/controllers/protected/CartController.js
@@ -0,0 +1,20 @@
+
+
+const createBundle = async(req,res)=> {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const createdBy = decodedToken.userId;
+    const {products, totalAmount} = req.body
+
+    
+
+
+
+}
+
+
+
+
+module.exports = {
+    createBundle
+};
