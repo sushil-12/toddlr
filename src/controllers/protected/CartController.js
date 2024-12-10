acdf7e68d1da7d359e1d175cce985fec4edcda7b
alpha2135 <begin2135@gmail.com>
2024-12-11 01:18:38 +0530

    create bundle api method created

--- src/controllers/protected/CartController.js
+++ src/controllers/protected/CartController.js
@@ -1,8 +1,28 @@
+const Bundle = require("../../models/Bundle");
+const { ErrorHandler, ResponseHandler } = require("../../utils/responseHandler");
+
 const createBundle = async(req,res)=> {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    const createdBy = decodedToken.userId;
-    const {products, totalAmount} = req.body
+
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const createdBy = decodedToken.userId;
+        const {products, totalAmount} = req.body
+    
+        const bundle = new Bundle({
+            createdBy,
+            products,
+            totalAmount
+        })
+        const savedBundle = await bundle.save()
+         // Respond with the newly created Bundle data
+         return ResponseHandler.success(res, savedBundle, 201, 'Bundle created successfully');
+    }
+    catch(error){
+        console.error(error);
+        ErrorHandler.handleError(error, res);
+    }
+
 }
 
 module.exports = {
